7c7fd12db0551991ee404dbe802777914411aac8
bhuvan1989 <chandbhuvan6@gmail.com>
2023-02-15 10:25:31 -0800

    mapvisit

--- pages/page/common/footer/components/Map.js
+++ pages/page/common/footer/components/Map.js
@@ -1,5 +1,6 @@
 import styled from "styled-components"
 
+
 const MapContainer = styled.a`
   display: flex;
   padding-bottom: 140px;
@@ -9,17 +10,29 @@ const MapContainer = styled.a`
   margin-left: 2rem;
   margin-bottom: 100px;
 `
-const Image = styled.img`
+const Image = styled.div`
   height: 200px;
   border: 3px solid #959595;
   border-radius: 20px;
-`
+overflow: hidden;  `
+
+  
 const Map = () => {
   return (
-    <MapContainer href="https://goo.gl/maps/CM2s4itnMDStmHVv8" target="_blank">
-      <Image src="/content/map1.png" />
+
+    <MapContainer>
+      <Image>
+
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3502.121357850357!2d77.37442991604617!3d28.62612468242031!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce5422cddcbd5%3A0xfabca9cd7a99f34d!2sRamaera%20Industries%20Ltd!5e0!3m2!1sen!2sin!4v1676369134463!5m2!1sen!2sin" 
+      height={"400px"}></iframe>
+      </Image>
+
+{/* width={310} height={250} style={{"marginTop":"10","border":"0"}} */}
+    
+   
     </MapContainer>
+
   )
 }
 
-export default Map
+export default Map;
