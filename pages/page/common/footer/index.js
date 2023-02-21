a95d69115c34ced6031c5299df86fdf5f85a1024
rajnishramaera <rajnish.it@ramaera.in>
2023-02-21 17:48:08 +0530

    kyc button added

--- pages/page/common/footer/index.js
+++ pages/page/common/footer/index.js
@@ -7,14 +7,28 @@ import Logo from "./components/Logo"
 import Map from "./components/Map"
 import Socials from "./components/Socials"
 import Foot from "./components/Foot"
+import { Sub, SubText, SubHeading, Input, DispFlex } from "./components/Links"
 
 const index = () => (
   <PageLayout bgColor="#000000" height="40vh">
     <PageWidth width="1500px" margin="0 0 0 0">
       <Responsive>
-        <Logo />
-        <Links />
-        <Map />
+        <DispFlex>
+          <Logo />
+          <Sub>
+            <SubHeading>Subscribe</SubHeading>
+            <form action="">
+              <Input placeholder="Email Adddress" />
+            </form>
+            <SubText>
+              Hello, we are Lift Media. Our goal is to translate the positive
+              effects from revolutionizing how companies engage with their
+              clients & their team.
+            </SubText>
+          </Sub>
+          {/* <Links /> */}
+          <Map />
+        </DispFlex>
         <Socials />
         <Foot />
       </Responsive>
