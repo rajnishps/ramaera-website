f7b2679af1bf3624e4de13722760d7501d820897
rajnishramaera <rajnish.it@ramaera.in>
2023-03-13 12:50:01 +0530

    ui form change

--- pages/page/Ideas/UserForm.js
+++ pages/page/Ideas/UserForm.js
@@ -7,70 +7,97 @@ export function UserForm({ yourName, citys, dob, occupation, contact }) {
   const [country, setCountry] = useState()
   return (
     <FormWrapper title="Basic Details">
-      <label>Your Name</label>
-      <input
-        autoFocus
-        required
-        type="text"
-        value={yourName}
-        placeholder="Type your name"
-      />
-
-      <label>Country</label>
-      <select
-        required
-        value={country}
-        onChange={(e) => setCountry(e.target.value)}
-      >
-        <option value="">Select Country Name</option>
-        {Country &&
-          Country.getAllCountries().map((item) => (
-            <option key={item.isoCode} value={item.isoCode}>
-              {item.name}
-            </option>
-          ))}
-      </select>
-
-      <label>State</label>
-      <select required value={state} onChange={(e) => setState(e.target.value)}>
-        <option value="">Select State Name</option>
-        {State &&
-          State.getStatesOfCountry(country).map((item) => (
-            <option key={item.isoCode} value={item.isoCode}>
-              {item.name}
-            </option>
-          ))}
-      </select>
-
-      <label>City</label>
-      <input required type="text" value={citys} />
-      <label>Date Of Birth</label>
-      <input required type="date" value={dob} />
-      <label>Your Occupation</label>
-      <select>
-        <option disabled selected>
-          Select Occupation
-        </option>
-        <option value={occupation}>Job</option>
-        <option value={occupation}>Business</option>
-      </select>
-
-      <label>Contact No</label>
-      <input
-        required
-        type="number"
-        value={contact}
-        placeholder="Type Contact No."
-      />
-
-      <label>Director Name</label>
-      <input autoFocus type="text" placeholder="Type your name" />
-
-      <label>Basic introduction with experience </label>
-      <input autoFocus type="text" />
-
-      <label>Role</label>
-      <input autoFocus type="text" />
+      <div style={{ width: "500px" }}>
+        <label>Your Name</label>
+        <br />
+        <input
+          autoFocus
+          required
+          type="text"
+          value={yourName}
+          placeholder="Type your name"
+        />
+      </div>
+      <div style={{ width: "450px" }}>
+        <label>Country</label>
+        <br />
+        <select
+          required
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        >
+          <option value="">Select Country Name</option>
+          {Country &&
+            Country.getAllCountries().map((item) => (
+              <option key={item.isoCode} value={item.isoCode}>
+                {item.name}
+              </option>
+            ))}
+        </select>
+      </div>
+      <div style={{ width: "500px" }}>
+        <label>State</label>
+        <br />
+        <select
+          required
+          value={state}
+          onChange={(e) => setState(e.target.value)}
+        >
+          <option value="">Select State Name</option>
+          {State &&
+            State.getStatesOfCountry(country).map((item) => (
+              <option key={item.isoCode} value={item.isoCode}>
+                {item.name}
+              </option>
+            ))}
+        </select>
+      </div>
+      <div style={{ width: "450px" }}>
+        <label>City</label>
+        <br />
+        <input required type="text" value={citys} />
+      </div>
+      <div style={{ width: "500px" }}>
+        <label>Date Of Birth</label>
+        <br />
+        <input required type="date" value={dob} />
+      </div>
+      <div style={{ width: "450px" }}>
+        <label>Your Occupation</label>
+        <br />
+        <select>
+          <option disabled selected>
+            Select Occupation
+          </option>
+          <option value={occupation}>Job</option>
+          <option value={occupation}>Business</option>
+        </select>
+      </div>
+      <div style={{ width: "500px" }}>
+        <label>Contact No</label>
+        <br />
+        <input
+          required
+          type="number"
+          value={contact}
+          placeholder="Type Contact No."
+        />
+      </div>
+      <div style={{ width: "450px" }}>
+        <label>Director Name</label>
+        <br />
+        <input autoFocus type="text" placeholder="Type your name" />
+      </div>
+      <div style={{ width: "500px" }}>
+        <label>Basic introduction with experience </label>
+        <br />
+        <input autoFocus type="text" />
+      </div>
+      <div style={{ width: "450px" }}>
+        <label>Role</label>
+        <br />
+        <input autoFocus type="text" />
+      </div>
     </FormWrapper>
   )
 }
