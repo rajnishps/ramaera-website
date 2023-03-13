7eb544b3b2ffe371d59bcb984d31933bfffc8622
Owais <owais.it@ramaera.in>
2023-03-13 12:39:15 +0530

    Ideas form

--- pages/page/Ideas/AddressForm.js
+++ pages/page/Ideas/AddressForm.js
@@ -1,4 +1,4 @@
-import { FormWrapper } from "./FormWrapper"
+import { FormWrapper } from "./FormWrapper";
 
 export function AddressForm({
   youAre,
@@ -32,88 +32,212 @@ export function AddressForm({
     <>
       <FormWrapper title="Project Details">
         <label>Your Project Type</label>
-        <input
-          autoFocus
-          required
-          type="text"
-          value={projectType}
-          // onChange={(e) => updateFields({ street: e.target.value })}
-        />
+        <select>
+          <option disabled selected>
+            Select Project Type
+          </option>
+          <option value={projectType}>Startup</option>
+          <option value={projectType}>Existing</option>
+        </select>
+
         <label>You Are ?</label>
-        <input required type="text" value={youAre} />
+        <select>
+          <option disabled selected>
+            Select
+          </option>
+          <option value={youAre}>Individual</option>
+          <option value={youAre}>Company</option>
+          <option value={youAre}>Registered NGO</option>
+        </select>
 
         <label>Type Of Partnership !!</label>
-        <input required type="text" value={partnership} />
+        <select>
+          <option disabled selected>
+            Select Partnership
+          </option>
+          <option value={partnership}>Equity Base</option>
+          <option value={partnership}>Debt/Loan</option>
+        </select>
 
         <label>Funding Type</label>
         <input required type="text" value={funding} />
 
         <label>Contribution Receiving Mode</label>
-        <input required type="text" value={contribution} />
+        <select>
+          <option disabled selected>
+            Select Project Type
+          </option>
+          <option value={contribution}>INR</option>
+        </select>
 
         <label>Goal (INR)</label>
-        <input required type="text" value={goal} />
+        <input
+          required
+          type="number"
+          value={goal}
+          placeholder="Numerical value only"
+        />
 
         <label>Target For</label>
-        <input required type="text" value={target} />
+        <input
+          required
+          type="number"
+          value={target}
+          placeholder="Number of Partners"
+        />
 
         <label>Per Person Amount</label>
         <input required type="text" value={personAmount} />
 
         <label>Your Project Title</label>
-        <input required type="text" value={projectTitle} />
+        <input
+          required
+          type="text"
+          value={projectTitle}
+          placeholder="I am offering for Partnership"
+        />
 
         <label>Brief about yourself & experience (minimum 30 character)*</label>
-        <input required type="text" value={brief} />
+        <textarea
+          required
+          value={brief}
+          rows={5}
+          cols={60}
+          placeholder="Brief about yourself..."
+        />
 
         <label>Market Problem</label>
-        <input required type="text" value={marketProblem} />
+        <textarea
+          required
+          value={marketProblem}
+          rows={5}
+          cols={60}
+          placeholder="Describe here..."
+        />
 
         <label>Solution</label>
-        <input required type="text" value={solution} />
+        <textarea
+          required
+          value={solution}
+          rows={5}
+          cols={60}
+          placeholder="Brief Story about your Solution/Product.      (Minimum 100 character)*"
+        />
 
         <label>Your Product</label>
-        <input required type="text" value={yourProduct} />
+        <textarea
+          required
+          value={yourProduct}
+          rows={5}
+          cols={60}
+          placeholder="How your Product help to peoples"
+        />
 
         <label>Business Model</label>
-        <input required type="text" value={businessModel} />
+        <textarea
+          required
+          value={businessModel}
+          rows={5}
+          cols={60}
+          placeholder="Discribe here ..."
+        />
 
         <label>Size of Market</label>
-        <input required type="text" value={sizeMarket} />
+        <textarea
+          required
+          value={sizeMarket}
+          rows={5}
+          cols={60}
+          placeholder="Discribe here size of the market you are targeting"
+        />
 
         <label>Current Trasaction</label>
-        <input required type="text" value={currentTrasaction} />
+        <textarea
+          required
+          value={currentTrasaction}
+          rows={5}
+          cols={60}
+          placeholder="Please discribe the current traction if any achivement you have already accomplished"
+        />
 
         <label>Competitive Landscape</label>
-        <input required type="text" value={competitiveLandscape} />
+        <textarea
+          required
+          rows={5}
+          cols={60}
+          value={competitiveLandscape}
+          placeholder="Discribe here your compatitors landscape"
+        />
 
         <label>Financial Projection</label>
-        <input required type="text" value={financialProjection} />
+        <textarea
+          required
+          rows={5}
+          cols={60}
+          value={financialProjection}
+          placeholder="Capex , Opex , Revenue"
+        />
 
         <label>Fund Need & Use</label>
-        <input required type="text" value={fundNeed} />
+        <textarea
+          required
+          rows={5}
+          cols={60}
+          value={fundNeed}
+          placeholder="Goal , and Use of fund"
+        />
 
         <label>Equity Structure</label>
-        <input required type="text" value={equityStructure} />
+        <textarea
+          required
+          rows={5}
+          cols={60}
+          value={equityStructure}
+          placeholder="Please discribe the Percentage (Funders %, Your %) after all expanses"
+        />
 
         <label>Exit Option</label>
-        <input required type="text" value={exitOption} />
+        <textarea
+          required
+          rows={5}
+          cols={60}
+          value={exitOption}
+          placeholder="Discribe here ..."
+        />
 
         <label>Select Fundraising Type</label>
-        <input required type="text" value={fundraisingType} />
+        <select>
+          <option disabled selected>
+            Select Mode
+          </option>
+          <option value={fundraisingType}>Equity Base</option>
+          <option value={fundraisingType}>Bank Loan</option>
+        </select>
 
         <label>Select Fundraising Stage</label>
-        <input required type="text" value={fundraisingStage} />
+        <select>
+          <option disabled selected>
+            Select Stage
+          </option>
+          <option value={fundraisingStage}>Pre-Seed</option>
+          <option value={fundraisingStage}>Seed</option>
+        </select>
 
         <label>Select Funding Type</label>
-        <input required type="text" value={fundingType} />
+        <select>
+          <option disabled selected>
+            Select Stage
+          </option>
+          <option value={fundingType}>Equity</option>
+          <option value={fundingType}>Debt</option>
+        </select>
 
         <label>Funding Amount</label>
         <input required type="text" value={fundingAmount} />
 
         <label>Funding Date</label>
-        <input required type="text" value={fundingDate} />
+        <input required type="date" value={fundingDate} />
       </FormWrapper>
     </>
-  )
+  );
 }
