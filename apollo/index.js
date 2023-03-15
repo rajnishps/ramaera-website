ad2c68c1b9030d38871ed62c71c1f49f8e534dd2
rajnishramaera <rajnish.it@ramaera.in>
2023-03-15 16:08:35 +0530

    routers to applicants

--- apollo/index.js
+++ apollo/index.js
@@ -1,8 +1,14 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
+let ACCESSTOKEN
 
+if (typeof window !== "undefined") {
+  ACCESSTOKEN = localStorage.getItem("accessToken")
+}
 const client = new ApolloClient({
   uri: "http://localhost:3000/graphql",
+  headers: {
+    authorization: `Bearer ${ACCESSTOKEN}`,
+  },
   cache: new InMemoryCache(),
 })
-
 export default client
