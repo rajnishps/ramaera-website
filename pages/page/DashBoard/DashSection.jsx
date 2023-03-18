815df96ccda4606bd522d7eac78127a85e6e25a1
Bhuvan Chand <bhuvan.it@ramaera.in>
2023-03-18 18:07:55 +0530

    dash section

--- pages/page/DashBoard/DashSection.jsx
+++ pages/page/DashBoard/DashSection.jsx
@@ -12,67 +12,71 @@ const DashSection = () => {
     <>
       <div className="container heading-container">
         <h1 className="heading-text">Dashboard Details </h1>
-        <label class="dropdown">
-          <div class="dd-button">Dropdown</div>
-          <AccountCircleIcon style={{ fontSize: "50px", color: "white" }} />
-          <input type="checkbox" class="dd-input" id="test" />
+        {/* <label class="dropdown"> */}
+        <div class="dd-button">Dropdown</div>
+        <AccountCircleIcon style={{ fontSize: "50px", color: "white" }} />
+        <input type="checkbox" class="dd-input" id="test" />
 
-          <ul class="dd-menu">
-            <li>Name</li>
-            <li class="divider"></li>
-            <li>Change Password</li>
-            <li class="divider"></li>
-            <li>Role</li>
-            <li className="divider"></li>
-            <li>Logout</li>
-          </ul>
-        </label>
+        <ul class="dd-menu">
+          <li>Name</li>
+          <li class="divider"></li>
+          <li>Change Password</li>
+          <li class="divider"></li>
+          <li>Role</li>
+          <li className="divider"></li>
+          <li>Logout</li>
+        </ul>
+        {/* </label> */}
       </div>
       <div className="container table-container">
         <div className="basis-2/12 table-2"></div>
         <div className="basis-8/12 table-8">
           <table id="myTable">
-            <tr className="header">
-              <th>DashBoard</th>
-              <th>Details</th>
-            </tr>
-            <tr>
-              <td>Application Data</td>
-              <td>
-                <Link href="/applicants" className="btn success">
-                  View All
-                </Link>
-              </td>
-            </tr>
-            <tr>
-              <td>Contact Data</td>
-              <td>
-                <Link href="/projectApplication" className="btn success">
-                  View All
-                </Link>
-              </td>
-            </tr>
-            <tr>
-              <td>Distributer Data</td>
-              <td>
-                <Link href="/contactDetail" className="btn success">
-                  View All
-                </Link>
-              </td>
-            </tr>
-            <tr>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-            </tr>
-            <tr>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-            </tr>
+            <thead>
+              <tr className="header">
+                <th>DashBoard</th>
+                <th>Details</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Application Data</td>
+                <td>
+                  <Link href="/applicants" className="btn success">
+                    View All
+                  </Link>
+                </td>
+              </tr>
+              <tr>
+                <td>Contact Data</td>
+                <td>
+                  <Link href="/projectApplication" className="btn success">
+                    View All
+                  </Link>
+                </td>
+              </tr>
+              <tr>
+                <td>Distributer Data</td>
+                <td>
+                  <Link href="/contactDetail" className="btn success">
+                    View All
+                  </Link>
+                </td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+              </tr>
+              <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+              </tr>
 
-            <tr>
-              <td>&nbsp;</td>
-              <td>&nbsp;</td>
-            </tr>
+              <tr>
+                <td>&nbsp;</td>
+                <td>&nbsp;</td>
+              </tr>
+            </tbody>
           </table>
         </div>
         <div className="basis-2/12 table-2"></div>
